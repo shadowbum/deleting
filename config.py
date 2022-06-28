4ea9f073e3748f7cb709b1d37a8e9c54c87a8163
Abolanosup <104039035+Abolanosup@users.noreply.github.com>
2022-06-28 10:44:48 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = #api id aqui
-api_hash = 'API_HASH'
-bot_token = 'BOT_TOKEN'
-useradm = 'ADMINUSER'
+api_id = 9902519
+api_hash = '9d8097d05bbc90a6ed2a7a81abcd4e8a'
+bot_token = '5371397546:AAGbQ00GUaKwOlU8r_H31MeJ8JUI8Xo118w'
+useradm = 'Abolanos3'

e32132610233c27b77cd7ade05281dc680396991
Abolanosup <104039035+Abolanosup@users.noreply.github.com>
2022-07-15 10:17:30 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 api_id = 9902519
 api_hash = '9d8097d05bbc90a6ed2a7a81abcd4e8a'
-bot_token = '5371397546:AAGbQ00GUaKwOlU8r_H31MeJ8JUI8Xo118w'
+bot_token = '5476727865:AAEtaUmWzYdubNebiwSy2MEosavJrnfYTG8'
 useradm = 'Abolanos3'

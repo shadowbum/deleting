a5d3f5845da96919933e5ce85d536a40f7065b53
shadowbum <110494804+shadowbum@users.noreply.github.com>
2022-08-05 08:33:39 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 api_id = 9902519
 api_hash = '9d8097d05bbc90a6ed2a7a81abcd4e8a'
-bot_token = '5476727865:AAEtaUmWzYdubNebiwSy2MEosavJrnfYTG8'
-useradm = 'Abolanos3'
+bot_token = '5512775702:AAEJRUmTg-uA2zmBkLe5umz9ELuhl-5WEl8'
+useradm = 'Roxanita00'
